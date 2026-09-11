dba1f72a045c52535d35c886ca51627a459cb330
Luizw3434 <carvalho.alves.luiz@escola.pr.gov.br>
2026-09-11 15:24:46 +0000

    finalizei

---
+++ atv10/script.js
@@ -0,0 +1,6 @@
+console.log('Luiz Felipe'); 
+console.log('projeto recupera back/front'); 
+console.log('seu nome é feio');
+console.log('seu nome é lindo');
+console.log('seu nome é diferente');
+console.log('olá vocês, boa sorte no projeto');
